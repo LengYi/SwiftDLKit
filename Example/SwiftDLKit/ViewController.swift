7cc9c72e2fe0d47fd94e5571eee563f9882ba74c
戴亮金 <dlj1@meitu.com>
2018-10-24 19:17:54 +0800

    添加String功能扩展，并测试成功

--- Example/SwiftDLKit/ViewController.swift
+++ Example/SwiftDLKit/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import SwiftDLKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        String.test()
     }
 
     override func didReceiveMemoryWarning() {
